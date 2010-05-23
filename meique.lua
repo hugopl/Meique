2edecb94704fbcb94fedec71674e2e124d1944fd
Hugo Parente Lima <hugo.pl@gmail.com>
2010-05-23 01:30:55 -0300

    I CAN BUILD MYSELF! huhuhuhuuhuhu
    Added meique.lua files to build meique itself :-D

---
+++ meique.lua
@@ -0,0 +1,2 @@
+lua = findPackage("lua")
+addSubDirectory("src")
---
+++ src/meique.lua
@@ -0,0 +1,19 @@
+meique = Executable:new("meique")
+meique:usePackage(lua)
+meique:addFiles([[main.cpp
+meique.cpp
+logger.cpp
+meiquescript.cpp
+config.cpp
+luastate.cpp
+stdstringsux.cpp
+compilerfactory.cpp
+gcc.cpp
+compileroptions.cpp
+linkeroptions.cpp
+target.cpp
+compilabletarget.cpp
+librarytarget.cpp
+customtarget.cpp
+os_unix.cpp
+filehash.cpp]])
