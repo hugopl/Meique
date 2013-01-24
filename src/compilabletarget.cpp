hugopl/Meique

commit 17e2c11b169f7ed3ead9e5fe73c613000d3a4add

Allow two targets to share source files.

src/compilabletarget.cpp
@@ -60,22 +60,22 @@ JobQueue* CompilableTarget::createCompilationJobs(Compiler* compiler, StringList
     std::string sourceDir = script()->sourceDir() + directory();
     std::string buildDir = OS::pwd();
 
-    StringList::const_iterator it = files.begin();
-    Language lang = identifyLanguage(*it);
+    Language lang = files.size() ? identifyLanguage(*files.begin()) : UnsupportedLanguage;
     m_linkerOptions->setLanguage(lang);
-    for (; it != files.end(); ++it) {
-        if (it->empty())
+
+    for (const std::string& fileName : files) {
+        if (fileName.empty())
             continue;
 
-        if (lang != identifyLanguage(*it))
+        if (lang != identifyLanguage(fileName))
             Error() << "You can't mix two programming languages in the same target!";
 
         // Create necessary directories if needed.
-        if (it->find_first_of('/'))
-            OS::mkdir(OS::dirName(*it));
+        if (fileName.find_first_of('/'))
+            OS::mkdir(OS::dirName(fileName));
 
-        std::string source = OS::normalizeFilePath(it->at(0) == '/' ? *it : sourceDir + *it);
-        std::string output = OS::normalizeFilePath(*it + ".o");
+        std::string source = OS::normalizeFilePath(fileName.at(0) == '/' ? fileName : sourceDir + fileName);
+        std::string output = OS::normalizeFilePath(fileName + "." + name() + ".o");
 
         bool compileIt = OS::timestampCompare(source, output) < 0;
         StringList dependents = getFileDependencies(source);
@@ -86,7 +86,7 @@ JobQueue* CompilableTarget::createCompilationJobs(Compiler* compiler, StringList
             OSCommandJob* job = compiler->compile(source, output, m_compilerOptions);
             job->addJobListenner(this);
             job->setWorkingDirectory(buildDir);
-            job->setName(*it);
+            job->setName(fileName);
             queue->addJob(job);
             m_job2Sources[job] = std::make_pair(source, dependents);
         }

tests/meique.lua
@@ -9,6 +9,7 @@ end
 
 local tests = [[
     two_targets_samename
+    two_targets_sharing_a_file
 ]]
 
 string.gsub(tests, '([^%s]+)', addMeiqueTest)

tests/two_targets_sharing_a_file/foo.cpp
@@ -1,0 +1,9 @@
+#include <stdio.h>
+
+#define API_EXPORT __attribute__ ((visibility("default")))
+
+API_EXPORT int foo(int bar)
+{
+    puts(TARGET_NAME);
+    return bar * 2;
+}

tests/two_targets_sharing_a_file/lib.cpp
@@ -1,0 +1,8 @@
+#define API_EXPORT __attribute__ ((visibility("default")))
+
+API_EXPORT int foo(int);
+
+API_EXPORT void justAnotherExportedFunction()
+{
+    foo(reinterpret_cast<unsigned long>(&foo));
+}

tests/two_targets_sharing_a_file/main.cpp
@@ -1,0 +1,6 @@
+int foo(int);
+
+int main()
+{
+    foo(42);
+}

tests/two_targets_sharing_a_file/meique.lua
@@ -1,0 +1,12 @@
+
+exe = Executable:new("exe")
+exe:addFiles("main.cpp foo.cpp")
+exe:addCustomFlags("-DTARGET_NAME='\"EXECUTABLE\"'")
+
+lib = Library:new("lib")
+lib:addFiles("lib.cpp foo.cpp")
+lib:addCustomFlags("-DTARGET_NAME='\"LIBRARY\"'")
+
+libUser = Executable:new("libUser")
+libUser:useTarget(lib)
+libUser:addFile("main.cpp")

tests/two_targets_sharing_a_file/script.sh
@@ -1,0 +1,14 @@
+$MEIQUE .. lib
+$MEIQUE exe
+
+if [ `./exe` != "EXECUTABLE" ]
+then
+    fail "main.cpp should use foo.cpp compiled with TARGET_NAME=EXECUTABLE!"
+fi
+
+$MEIQUE libUser
+
+if [ `./libUser` != "LIBRARY" ]
+then
+    fail "main.cpp on libUser should use foo.cpp compiled with TARGET_NAME=LIBRARY!"
+fi

commit 1f4a9ee3802e8660f87055dc9a0cf91eee5f9e23

Fix regression on "meique -c" caused by 17e2c11b169f7ed3ead9e5fe73c613000d3a4add.

src/compilabletarget.cpp
@@ -281,9 +281,13 @@ void CompilableTarget::clean()
     getLuaField("_files");
     StringList files;
     readLuaList(luaState(), lua_gettop(luaState()), files);
-    StringList::const_iterator it = files.begin();
-    for (; it != files.end(); ++it)
-        OS::rm(directory() + *it + ".o");
+
+    for (std::string fileName : files) {
+        fileName += "." + name() + ".o";
+        if (fileName[0] != '/')
+            fileName.insert(0, directory());
+        OS::rm(fileName);
+    }
 }
 
 void CompilableTarget::doTargetInstall(const std::string& destDir)