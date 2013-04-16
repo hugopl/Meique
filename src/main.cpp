189a9d204cfe9563c80177f29d8deeef713847bd
Hugo Parente Lima <hugo.pl@gmail.com>
2013-04-15 23:04:37 -0300

    Remove debug switch used to throw exception when in debug mode.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /*
     This file is part of the Meique project
-    Copyright (C) 2009-2010 Hugo Parente Lima <hugo.pl@gmail.com>
+    Copyright (C) 2009-2013 Hugo Parente Lima <hugo.pl@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,21 +16,15 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <iostream>
 #include "meique.h"
 #include "logger.h"
 
 int main(int argc, const char** argv)
 {
-#ifndef NDEBUG
-    Meique meique(argc, argv);
-    meique.exec();
-#else
     try {
         Meique meique(argc, argv);
         meique.exec();
     } catch (MeiqueError&) {
         return 1;
     }
-#endif
 }
