a88e7546d31a8bb1d5592c6181266fbdeafe6c44
Hugo Parente Lima <hugo.pl@gmail.com>
2014-02-01 02:05:34 -0300

    Update copyright year.

--- src/meique.cpp
+++ src/meique.cpp
@@ -1,6 +1,6 @@
 /*
     This file is part of the Meique project
-    Copyright (C) 2009-2013 Hugo Parente Lima <hugo.pl@gmail.com>
+    Copyright (C) 2009-2014 Hugo Parente Lima <hugo.pl@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -326,7 +326,7 @@ void Meique::exec()
 int Meique::showVersion()
 {
     std::cout << "Meique version " MEIQUE_VERSION << std::endl;
-    std::cout << "Copyright 2009-2013 Hugo Parente Lima <hugo.pl@gmail.com>\n";
+    std::cout << "Copyright 2009-2014 Hugo Parente Lima <hugo.pl@gmail.com>\n";
     return 0;
 }
 
