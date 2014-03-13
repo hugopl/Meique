e8228420c26699b3d2a714c03c71f5c261eb433d
Hugo Parente Lima <hugo.pl@gmail.com>
2014-03-13 17:10:21 -0300

    Build meique as a static lib, so we can use this lib on unit tests.

--- src/meique.lua
+++ src/meique.lua
@@ -1,11 +1,10 @@
-
-meique = Executable:new("meique")
+-- Build meique as a static lib, so we can use this lib on unit tests.
+meiqueLib = Library:new("meiquelib", STATIC)
 GCC:addCustomFlags("-std=c++0x")
-meique:use(lua)
-meique:addLinkLibraries("pthread")
-meique:addFiles([[
+meiqueLib:use(lua)
+meiqueLib:addLinkLibraries("pthread")
+meiqueLib:addFiles([[
 dependencechecker.cpp
-main.cpp
 meiqueregex.cpp
 meique.cpp
 nodetree.cpp
@@ -29,10 +28,10 @@ luajob.cpp
 luacpputil.cpp
 ]])
 
-meique:addFiles(meique:buildDir().."meiqueapi.cpp")
+meiqueLib:addFiles(meiqueLib:buildDir().."meiqueapi.cpp")
 
 meiqueApi = CustomTarget:new("meiqueapi", function(modifiedFiles)
-    local out = meique:buildDir().."meiqueapi.cpp"
+    local out = meiqueLib:buildDir().."meiqueapi.cpp"
     local file2cBin = file2c:buildDir().."file2c"
     local res = os.execute(file2cBin.." meiqueApi "..modifiedFiles[0].." > "..out)
     abortIf(res ~= 0, "Error creating "..out..".")
@@ -40,6 +39,10 @@ end)
 meiqueApi:addFile("meiqueapi.lua")
 meiqueApi:addOutput("meiqueapi.cpp")
 
-meique:addDependency(meiqueApi)
+meiqueLib:addDependency(meiqueApi)
 meiqueApi:addDependency(file2c)
+
+meique = Executable:new("meique")
+meique:addFile("main.cpp")
+meique:use(meiqueLib)
 UNIX:meique:install("bin")
